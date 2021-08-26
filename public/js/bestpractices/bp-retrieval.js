38edda31c765388d304a73dbccbe612f701826b2
Hamburger2k <59456376+Hamburger2k@users.noreply.github.com>
2021-08-26 18:31:43 +0200

    Added base for filtering setup and other updates
    - Package updates
    - Using jsonmodel over jsontest
    - Dropdown menu in create instance for filters
    - UI for filters in bp viewing
    - Predefined option selection for creating best practices

--- public/js/bestpractices/bp-retrieval.js
+++ public/js/bestpractices/bp-retrieval.js
@@ -24,15 +24,28 @@ var catKeyName;
 
 // Array of all categories
 var catArray = [];
+var datatype = [];
+
+// Temporary variables to use for creating sliders, based on existing values. Should be removed in the full system.
+var alreadytimeframe;
+var alreadyeffortFrame;
+
+// Get the modal
+var modalfilter = document.getElementById("FilterModal");
+
+// Get the button that opens the modal
+var btnfilter = document.getElementById("FilterBtn");
+
+// Get the <span> element that closes the modal
+var spanfilter = document.getElementsByClassName("closefilter")[0];
+
 
 // Function for filtering out duplicate array values
 const unique = (value, index, self) => {
   return self.indexOf(value) === index;
 };
 
-
 // ############################################
-
 
 // Call the dataTables jQuery plugin
 // Necessary for correctly displaying data in the table
@@ -113,18 +126,114 @@ async function getDocData(callback) {
     .where("created", "==", "true")
       .get().then((snapshot) => {
           snapshot.docs.forEach(doc => {
-
-            // Emptying both arrays so that they don't increment for each doc in the snapshot
             keyArray = [];
             indexArr = [];
 
-            docIDs.push(doc.id);
+            alreadyeffortFrame = false;
+            alreadytimeframe = false;
 
+            docIDs.push(doc.id);
+            var selection = document.getElementById("filterbtns");
+            selection.innerHTML = "";
             // Pushing key names (without numerics) to keyArray
             // We want to display title, description and date > using this array, we can find the index of those keys in the documents
             for (let key in doc.data()) {
               keyArray.push(JSON.stringify(key).replace(/[ˆ0-9]+/g, ''));
               keyArrayWithNum.push(key);
+              normalkey = JSON.stringify(key).replace(/[ˆ0-9]+/g, '');
+              normalkey = normalkey.replace(/['"]+/g, '');
+              
+              snapshot.docs.forEach(doc => {
+              datatype.push(doc.data()[key]);  
+              });
+
+
+              var label = document.createElement("label");
+              if(datatype.length > 0){
+                datatype = datatype.filter(unique);
+                datatype.forEach(attribute => {   
+                  if(!(attribute === undefined || attribute == "" )){
+                    
+                    if(!(normalkey == "categories" || normalkey == "title" || normalkey == "description" || normalkey == "author" || normalkey == "created" || normalkey == "lessonslearned" || normalkey == "problems" || normalkey == "effort" || normalkey == "timeframe" || normalkey == "date")){
+                      
+                    var description = document.createTextNode("  " + attribute + "   ");
+                    var checkbox = document.createElement("input");
+                    
+      
+                    checkbox.type = "checkbox";    // make the element a checkbox
+                    checkbox.name = "slct[]";      // give it a name we can check on the server side
+                    checkbox.value = attribute;         // make its value "pair"
+                    checkbox.style.padding = "20px";
+                    checkbox.style.backgroundColor = "red";
+      
+                    
+                    checkbox.style.left =  "20px";
+                    checkbox.style.bottom=  "20px";
+                    checkbox.style.width = "20px";
+                    checkbox.style.height = "20px";
+                    checkbox.style.border = "solid white";
+                    checkbox.style.borderWidth = "20px";
+                    label.style.padding = "5px";
+                    
+                    
+                    
+      
+                    label.appendChild(checkbox);   // add the box to the element
+                    label.appendChild(description);// add the description to the element
+                    //label.title = "test";
+      
+                    // add the label element to your div
+                    }
+                    if(normalkey == "effort"){
+                      if(alreadyeffortFrame) return;
+
+                    
+                    var slider = document.createElement("input");
+                    slider.type = "range";
+                    slider.max = "10";
+                    slider.min = "1";
+                    label.appendChild(slider);   // add the box to the element
+                    alreadyeffortFrame = true;
+                    
+                      
+                    }
+                    if(normalkey == "timeframe"){
+                      if(alreadytimeframe) return;
+                    var slider = document.createElement("input");
+                    slider.type = "range";
+                    slider.max = "10";
+                    slider.min = "1";
+                    label.appendChild(slider);   // add the box to the element
+                                   
+                    alreadytimeframe = true;
+                    }
+                  }
+                  // if(normalkey == "timeframe"){
+                  //   alreadytimeframe = true;
+                  //   console.log("c");
+                  // }
+                  });
+                  if(!(normalkey == "categories" || normalkey == "date" || normalkey == "title" || normalkey == "description" || normalkey == "author" || normalkey == "created" || normalkey == "lessonslearned" || normalkey == "problems" )){   
+                  var div = document.createElement('div');
+                  div.id = 'container';
+                  div.innerHTML = normalkey;
+                  //div.className = 'border pad';
+                  div.style.fontFamily =    "Arial, Helvetica, sans-serif";
+                  div.style.fontSize =  "20px";
+                  div.style.fontWeight =    "bold";
+                  
+                  selection.appendChild(div);
+                  selection.appendChild(label); 
+
+                  
+                  
+                  }
+                  
+                
+                    
+              }
+            
+              datatype = [];
             }
 
             for(let x=0; x<keyArray.length; x++){
@@ -158,12 +267,14 @@ async function getDocData(callback) {
             data.push(docdata);
 
             // Populating the category array
-            doc.data()[Object.keys(doc.data())[categories]].forEach(element => {
-              catArray.push(element);
+            snapshot.docs.forEach(doc => {   
+              
+              catArray.push(doc.data()["6categories"]);;
             });
 
           });
       });
+    
 
   await delay();
   callback();
@@ -179,6 +290,34 @@ function populateCat() {
   // Iterating over the categories of retrieved BPs
   uniqueCat.forEach(categoryElement => {
 
+    if(!(categoryElement === undefined || categoryElement == "")){
+
+      var label = document.createElement("label");
+      var description = document.createTextNode("  " + categoryElement + "   ");
+      var checkbox = document.createElement("input");
+      
+      // Creates the checkboxes for categories in the filter modal
+      checkbox.type = "checkbox";    
+      checkbox.name = "slct" + categoryElement;      
+      checkbox.value = categoryElement;         
+      checkbox.style.padding = "20px";
+      checkbox.style.left =  "20px";
+      checkbox.style.bottom=  "20px";
+      checkbox.style.width = "20px";
+      checkbox.style.height = "20px";
+      checkbox.style.border = "solid white";
+      checkbox.style.borderWidth = "20px";
+      label.style.padding = "5px";
+
+      label.appendChild(checkbox);   // add the box to the element
+      label.appendChild(description);// add the description to the element
+  
+      // add the label element to your div
+      document.getElementById('some_div').appendChild(label);
+      
+      }
+
+
     let category = document.createElement('option');
     category.setAttribute('value', categoryElement);
     category.textContent = categoryElement;
@@ -189,12 +328,15 @@ function populateCat() {
 
 }
 
+$("#some_div").on("click", ".checkboxes", function () {
+  var value=$(this).attr("value");
+  alert(value);
+});
 
 // When a category is selected
 $("#category-select").change(function() {
 
   docIDs = [];
-
   const selectedCat = document.getElementById("category-select").value;
 
   if(selectedCat == ''){
@@ -213,9 +355,7 @@ $("#category-select").change(function() {
                 // Pushing data to docdata array to populate the table
                 let docdata = [`${doc.data()[Object.keys(doc.data())[title]]}`, `${doc.data()[Object.keys(doc.data())[description]]}`, `${doc.data()[Object.keys(doc.data())[date]]}`];
                 data.push(docdata);
-
                 docIDs.push(doc.id);
-
             });
 
             // DataTable needs to be destroyed before reinitializing
@@ -241,5 +381,64 @@ $("#category-select").change(function() {
         });
   }
   catArray = [];
-
 });
+
+btnfilter.onclick = function() {
+  modalfilter.style.display = "block";
+}
+
+  function apply(){
+    datatype = [];
+  }
+
+  function setValue(key){
+    // bpPath is the collection path to the bestpractices sub-collection
+    if(datatype.length < 1){
+    let bpPath = findPath(collectionPaths, 'bestpractices');
+    //Getting all best practice documents
+    db.collection(`${bpPath}`)
+    .where("created", "==", "true")
+        .get().then((snapshot) => {
+            snapshot.docs.forEach(doc => {
+              datatype.push(doc.data()[key]);                       
+          });
+        });
+      } 
+
+        var selectsection = document.getElementById("selectionsection");
+        datatype = datatype.filter(unique);
+        document.getElementById("selectionsection").innerHTML = "";
+  
+        if(!(datatype === undefined)){
+            datatype.forEach(attribute => {
+              if(!(attribute === undefined || attribute == "")){
+              
+                var label = document.createElement("label");
+                var description = document.createTextNode("  " + attribute + "   ");
+                var checkbox = document.createElement("input");
+                
+                checkbox.type = "checkbox";   
+                checkbox.name = "slct" + attribute;    
+                checkbox.value = attribute;   
+                checkbox.style.padding = "20px";
+                checkbox.style.left =  "20px";
+                checkbox.style.bottom=  "20px";
+                checkbox.style.width = "20px";
+                checkbox.style.height = "20px";
+                checkbox.style.border = "solid white";
+                checkbox.style.borderWidth = "20px";
+                label.style.padding = "5px";
+                
+                label.appendChild(checkbox);    // add the box to the element
+                label.appendChild(description); // add the description to the element
+             
+                selectsection.appendChild(label); 
+            }
+          }
+        )
+      }
+    }
+
+  spanfilter.onclick = function() {
+    modalfilter.style.display = "none";
+  }
